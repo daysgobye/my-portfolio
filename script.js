8ce0c37a0dddf77ee91b568b468dd0f442c4dc2a
cole smith <smithcole@protoanmail.com>
2018-07-22 22:02:40 -0600

    added files for my owrk page and for contact page

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+const hammy = document.querySelector('.headder__hamburger')
+const nav = document.querySelector('.nav')
+const headHam = document.querySelector('.headder__hamburger')
+const ham3 = document.querySelector('.headder__hamburger__3')
+const ham1 = document.querySelector('.headder__hamburger__1')
 
 function hamburger__clicked() {
-  var about = document.querySelector('.headder__hamburger__1')
-  about.classList.toggle ("headder__hamburger__1__clicked")
-  var about = document.querySelector('.headder__hamburger__3')
-  about.classList.toggle("headder__hamburger__3__clicked")
-  var about = document.querySelector('.headder__hamburger')
-  about.classList.toggle("headder__hamburger__clicked")
-  var about = document.querySelector('.nav')
-  about.classList.toggle ("nav__clicked")
+  ham1.classList.toggle ("headder__hamburger__1__clicked")
+  ham3.classList.toggle("headder__hamburger__3__clicked")
+  headHam.classList.toggle("headder__hamburger__clicked") 
+  nav.classList.toggle ("nav__clicked")
 }
+hammy.addEventListener('click',hamburger__clicked)
