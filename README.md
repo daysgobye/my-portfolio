4443960b96a367b327d80f55f78d01fb789c4403
cole smith <smithcole@protoanmail.com>
2018-07-22 21:31:40 -0600

    total rework going for somthing better

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-portfolio
-just starting on this site
---
+++ script.js
@@ -0,0 +1,11 @@
+
+function hamburger__clicked() {
+  var about = document.querySelector('.headder__hamburger__1')
+  about.classList.toggle ("headder__hamburger__1__clicked")
+  var about = document.querySelector('.headder__hamburger__3')
+  about.classList.toggle("headder__hamburger__3__clicked")
+  var about = document.querySelector('.headder__hamburger')
+  about.classList.toggle("headder__hamburger__clicked")
+  var about = document.querySelector('.nav')
+  about.classList.toggle ("nav__clicked")
+}
