7a312def10b6351317c8bfa96e7445ae9ceaa993
cole smith <smithcole@protoanmail.com>
2018-07-23 15:18:14 -0600

    got the contact page working

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const ham3 = document.querySelector('.headder__hamburger__3')
 const ham1 = document.querySelector('.headder__hamburger__1')
 
 function hamburger__clicked() {
-  console.log('hey');
-  
   ham1.classList.toggle ("headder__hamburger__1__clicked")
   ham3.classList.toggle("headder__hamburger__3__clicked")
   headHam.classList.toggle("headder__hamburger__clicked") 
