5a2bb7a2c494780e9fadd987f52dd3327f58b089
cole smith <smithcole@protoanmail.com>
2018-07-23 21:36:15 -0600

    got my work page up and running

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ const nav = document.querySelector('.nav')
 const headHam = document.querySelector('.headder__hamburger')
 const ham3 = document.querySelector('.headder__hamburger__3')
 const ham1 = document.querySelector('.headder__hamburger__1')
-
+const list = document.querySelectorAll('li')
+const mywork = document.querySelector('.mywork__site')
+let lastLi = 1
+let lastpicked;
+function portPicker(){
+  if(lastLi != 1){
+    lastLi.classList.remove("slected")
+  }
+  this.classList.add("slected")
+  lastLi = this
+  let picked = this.innerHTML
+  
+  mywork.classList.remove(`${lastpicked}`)
+  mywork.classList.add("mywork__site",`${picked}`)
+  lastpicked = picked
+}
 function hamburger__clicked() {
   ham1.classList.toggle ("headder__hamburger__1__clicked")
   ham3.classList.toggle("headder__hamburger__3__clicked")
@@ -11,3 +26,4 @@ function hamburger__clicked() {
   nav.classList.toggle ("nav__clicked")
 }
 hammy.addEventListener('click',hamburger__clicked)
+list.forEach(item => item.addEventListener('click',portPicker))
